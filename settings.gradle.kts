eec053e404c233a7d9c48463aae82d51070bcce0
Sascha Lisson <sl@slisson.de>
2022-12-15 09:29:42 +0100

    use kotlin plugin version from gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("multiplatform") version kotlinVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+    }
+    resolutionStrategy {
+    }
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        maven { url = uri("https://artifacts.itemis.cloud/repository/maven-mps/") }
+        mavenCentral()
+    }
+}
+
 rootProject.name = "modelix.kernelf"
 
 include("kernelf-angular-demo")
