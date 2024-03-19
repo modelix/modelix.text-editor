1afbcc63a8eff9f0b7cc1952111c1792bb9fa4c8
Sascha Lisson <sascha.lisson@itemis.de>
2024-03-19 09:48:25 +0100

    ci: workflows for automated releases

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "deps",
+        "projectional-editor",
+        "mps-plugin",
+      ],
+    ],
+    "subject-case": [0, 'never'],
+    "body-max-line-length": [0, 'always'],
+    "footer-max-line-length": [0, 'always']
+  },
+  ignores: [
+    (message) => message.includes('skip-lint')
+  ],
+};
