ba6e89b533e31a4bd8bd518c8f59d9df7e25eb78
Sascha Lisson <sascha.lisson@itemis.de>
2024-07-25 14:55:36 +0200

    fix(projectional-editor): kernelf editor fixes

--- kernelf-editor/src/commonMain/kotlin/org/modelix/editor/kernelf/Editor_org_iets3_core_expr_datetime.kt
+++ kernelf-editor/src/commonMain/kotlin/org/modelix/editor/kernelf/Editor_org_iets3_core_expr_datetime.kt
@@ -4,5 +4,4 @@ import org.iets3.core.expr.datetime.L_org_iets3_core_expr_datetime
 import org.modelix.aspects.languageAspects
 
 val Editor_org_iets3_core_expr_datetime = languageAspects(L_org_iets3_core_expr_datetime) {
-
 }
