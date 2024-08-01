a06f4c64da3300465579ac9b4d60e916c900320a
Sascha Lisson <sascha.lisson@itemis.de>
2024-08-01 21:24:24 +0200

    feat: parser

--- projectional-editor/src/commonMain/kotlin/org/modelix/editor/celltemplate/IGrammarSymbol.kt
+++ projectional-editor/src/commonMain/kotlin/org/modelix/editor/celltemplate/IGrammarSymbol.kt
@@ -1,6 +1,8 @@
 package org.modelix.editor.celltemplate
 
+import org.modelix.editor.IActionOrProvider
 import org.modelix.editor.ICodeCompletionAction
+import org.modelix.editor.TemplateCellReference
 import org.modelix.model.api.IChildLink
 import org.modelix.model.api.INode
 
@@ -8,6 +10,8 @@ interface IGrammarSymbol {
     fun createWrapperAction(nodeToWrap: INode, wrappingLink: IChildLink): List<ICodeCompletionAction> {
         return emptyList()
     }
+
+    fun getSymbolTransformationAction(node: INode, optionalCell: TemplateCellReference): IActionOrProvider?
 }
 
 interface IOptionalSymbol : IGrammarSymbol {
