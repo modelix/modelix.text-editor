ec9b0b393b514b939b22e4379deb3f3bc95ad41c
Sascha Lisson <sascha.lisson@itemis.de>
2024-10-18 15:27:19 +0200

    feat(interpreter): revert deletion

---
+++ interpreter-vm/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("multiplatform")
+    `maven-publish`
+}
+
+kotlin {
+    jvmToolchain(17)
+    jvm()
+    js(IR) {
+        browser {}
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "10s"
+                }
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(coreLibs.kotlin.collections.immutable)
+                implementation(coreLibs.modelix.incremental)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+            }
+        }
+    }
+}
---
+++ interpreter-vm/src/commonMain/kotlin/org/modelix/interpreter/vm/core/ProgramBuilder.kt
@@ -0,0 +1,54 @@
+package org.modelix.interpreter.vm.core
+
+import kotlin.reflect.KProperty
+
+class ProgramBuilder {
+
+    private val functions: MutableMap<Any, FunctionBuilder> = HashMap()
+
+    fun getFunction(key: Any): FunctionBuilder {
+        return checkNotNull(functions[key]) { "Function doesn't exist: $key" }
+    }
+
+    fun getOrBuildFunction(key: Any, body: FunctionBuilder.() -> Unit): FunctionBuilder {
+        return functions[key] ?: buildFunction(key, body)
+    }
+
+    fun buildFunction(key: Any, body: FunctionBuilder.() -> Unit): FunctionBuilder {
+        check(functions[key] == null) { "Function already exists: $key" }
+        val builder = FunctionBuilder()
+        functions[key] = builder
+        body(builder)
+        return builder
+    }
+
+    fun <T> variable(type: MemoryType = MemoryType.LOCAL) = O<T>(type)
+}
+
+class O<T>(private val type: MemoryType) {
+    private var instance: MemoryKey<T>? = null
+    operator fun getValue(thisRef: Nothing?, property: KProperty<*>): MemoryKey<T> {
+        return instance ?: MemoryKey<T>(type, property.name).also { instance = it }
+    }
+}
+
+class FunctionBuilder {
+    private var firstInstruction: Instruction? = null
+    private var lastInstruction: Instruction? = null
+
+    fun addInstruction(instruction: Instruction) {
+        lastInstruction?.next = instruction
+        lastInstruction = instruction
+        if (firstInstruction == null) {
+            firstInstruction = instruction
+        }
+    }
+
+    fun getEntryPoint(): Instruction {
+        return firstInstruction ?: NoOpInstruction().also { firstInstruction = it }
+    }
+
+    fun <T> load(value: T, variable: MemoryKey<T>) {
+        addInstruction(LoadConstantInstruction(value, variable))
+    }
+}
