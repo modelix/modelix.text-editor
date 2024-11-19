3f6e41ef3f11f22668e52b6eb0fefc3e4565239e
slisson <sl@q60.de>
2024-11-19 14:04:33 +0100

    fix: enable publishing to itemis nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,24 +66,24 @@ subprojects {
         mavenCentral()
     }
 
-//    publishing {
-//        repositories {
-//            if (project.hasProperty("artifacts.itemis.cloud.user")) {
-//                maven {
-//                    name = "itemis"
-//                    url = if (version.toString().contains("SNAPSHOT")) {
-//                        uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
-//                    } else {
-//                        uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
-//                    }
-//                    credentials {
-//                        username = project.findProperty("artifacts.itemis.cloud.user").toString()
-//                        password = project.findProperty("artifacts.itemis.cloud.pw").toString()
-//                    }
-//                }
-//            }
-//        }
-//    }
+    publishing {
+        repositories {
+            if (project.hasProperty("artifacts.itemis.cloud.user")) {
+                maven {
+                    name = "itemis"
+                    url = if (version.toString().contains("SNAPSHOT")) {
+                        uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
+                    } else {
+                        uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
+                    }
+                    credentials {
+                        username = project.findProperty("artifacts.itemis.cloud.user").toString()
+                        password = project.findProperty("artifacts.itemis.cloud.pw").toString()
+                    }
+                }
+            }
+        }
+    }
 }
 
 subprojects {
