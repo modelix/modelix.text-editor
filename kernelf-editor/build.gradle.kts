ce850bf61eef5664da8681366afc5d7ea28e00f4
Sascha Lisson <sl@q60.de>
2025-04-13 18:15:49 +0200

    test: increased timeout

--- kernelf-editor/build.gradle.kts
+++ kernelf-editor/build.gradle.kts
@@ -29,7 +29,7 @@ kotlin {
         browser {
             testTask {
                 useMocha {
-                    timeout = "60s"
+                    timeout = "120s"
                 }
             }
         }
@@ -37,7 +37,7 @@ kotlin {
         nodejs {
             testTask {
                 useMocha {
-                    timeout = "60s"
+                    timeout = "120s"
                 }
             }
         }
